0d3132898be7e0e018df21985ec5cfd8fa7e48d2
Nikolakito <nikolakitoferreiros@gmail.com>
2024-06-06 18:45:06 -0300

    Llegue hasta 2.10

---
+++ scripts/comparisions.js
@@ -0,0 +1,8 @@
+//What will be the result for these expressions?
+alert(5 > 4); //T
+alert("apple" > "pineapple"); //F
+alert("2" > "12"); //T
+alert(undefined == null); //T
+alert(undefined === null); //F
+alert(null == "\n0\n"); //F
+alert(null === +"\n0\n"); //F
---
+++ scripts/postNPrefixTerms.js
@@ -0,0 +1,11 @@
+//What are the final values of all variables a, b, c and d after the code below?
+
+let a = 1, b = 1;
+
+let c = ++a; // 2
+let d = b++; // 1
+
+alert(a);
+alert(b);
+alert(c);
+alert(d);
