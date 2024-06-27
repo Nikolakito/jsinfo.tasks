cf8b25823ea185893323f7d3bdae9ecf04ee42c9
Nikolakito <nikolakitoferreiros@gmail.com>
2024-06-27 18:53:13 -0300

    llegue hasta while y for

---
+++ scripts/lastLoopValue.js
@@ -0,0 +1,37 @@
+/*What is the last value alerted by this code? Why?
+
+let i = 3;
+
+while (i) {
+  alert( i-- );
+}
+(1)
+
+For every loop iteration, write down which value it outputs and then compare it with the solution.
+
+Both loops alert the same values, or not?
+
+The prefix form ++i:
+
+let i = 0;
+while (++i < 5) alert( i );
+(4)
+The postfix form i++
+
+let i = 0;
+while (i++ < 5) alert( i );
+(5)
+
+For each loop write down which values it is going to show. Then compare with the answer.
+
+Both loops alert same values or not?
+
+The postfix form:
+
+for (let i = 0; i < 5; i++) alert( i );
+The prefix form:
+
+for (let i = 0; i < 5; ++i) alert( i );
+(van a devolver los mismos valores)
+*/
+
